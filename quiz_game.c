5a7379e5578f5578f0c00c85f49c0aa6a4107c58
Piyushkumar32 <shalendrashalendra869@gmail.com>
2025-04-15 08:27:49 -0700

    Add files via upload

---
+++ quiz_game.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<ctype.h>
+int main(){
+    char question[][100]={"A.What is thhe capital of india ?",
+                          "B.Who is the prime minister  of india?",
+                          "C.Which is longest river of india ?"};
+    char option[][100]={"A.Kolkata ","B.Delhi","C.Bengaluru","D.Assam"
+                         ,"A.Rahul gandhi","B.Narandra modi","C.Lallu yadav","D.Mamta banerjee"
+                         ,"A.Yamuna","B.Brahmputra","C.Ganga","D.Narmada"};
+    char Answer[]={'B','B','C','a','d'};
+    int nof=sizeof(question)/sizeof(question[0]);
+    char ans;
+    int score =0;
+    printf("The Quiz Game:\n");
+    for (int  i = 0; i < nof; i++)
+    {
+        printf("*********\n");
+        printf("%s\n",question[i]);
+        printf("*********\n");
+        for (int j = (i*4); j < (i*4)+4; j++)
+        {
+           printf("%s\n",option[j]);
+        }
+        printf("ANSWER:");
+        scanf(" %c",&ans);
+        ans=toupper(ans);
+        if ( ans == Answer[i])
+        {
+            printf("Correct Answer\n");
+            score++;
+        }else if (ans != Answer[0]||ans != Answer[2]||ans != Answer[3]||ans != Answer[4])
+        {
+            printf("invalid option");
+        }
+        
+        else
+        {
+            printf("incorrect Answer\n");
+        }
+        
+        
+    }
+    if (score == nof)
+    {
+        printf("You answer all of them correctly \n");
+        
+    printf("Score:");
+    printf("%d/%d", score,  nof);
+
+    }else
+    {
+        printf("Better luck nnext time \n");
+        
+    printf("Score:");
+    printf("%d/%d", score,  nof);
+    }
+    
+    
+                                              
+}
